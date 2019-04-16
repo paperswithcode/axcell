18f731bc30e8e24a2753ac0726eda16c31250409
Marcin <marcin.kardas@gmail.com>
2019-04-16 11:46:50 +0200

    Generate htmls with make

--- Makefile
+++ Makefile
@@ -1,10 +1,28 @@
 DATA_DIR = ../data
 ANNOTATIONS_DIR = $(DATA_DIR)/annotations
-ARCHIVES_DIR = $(DATA_DIR)/sources
+ARCHIVES_DIR = $(DATA_DIR)/arxiv/sources
+UNPACKED_DIR = $(DATA_DIR)/arxiv/unpacked_sources
+HTMLS_DIR = $(DATA_DIR)/arxiv/htmls
+
+ARCHIVES = $(wildcard $(ARCHIVES_DIR)/*)
+UNPACKS  = $(patsubst $(ARCHIVES_DIR)/%,$(UNPACKED_DIR)/%,$(ARCHIVES))
+HTMLS    = $(patsubst $(ARCHIVES_DIR)/%,$(HTMLS_DIR)/%.html,$(ARCHIVES))
+
 $(shell mkdir -p "$(DATA_DIR)")
+$(shell mkdir -p "$(HTMLS_DIR)")
 
 .PHONY: all
-all:	$(ANNOTATIONS_DIR)/pdfs-urls.csv $(ANNOTATIONS_DIR)/sources-urls.csv
+all:	$(ANNOTATIONS_DIR)/pdfs-urls.csv $(ANNOTATIONS_DIR)/sources-urls.csv convert_all
+
+convert_all: $(HTMLS)
+
+$(HTMLS): $(HTMLS_DIR)/%.html: $(UNPACKED_DIR)/%
+	./docker-latex2html.sh $(HTMLS_DIR) $^
+
+unpack_all: $(UNPACKS)
+
+$(UNPACKS): $(UNPACKED_DIR)/%: $(ARCHIVES_DIR)/%
+	./unpack-sources.sh $^ $@
 
 $(ANNOTATIONS_DIR)/pdfs-urls.csv:	$(ANNOTATIONS_DIR)/papers-urls.csv
 	sed -e 's#/abs/#/pdf/#' -e 's#$$#.pdf#' $^ > $@
--- unpack-sources.sh
+++ unpack-sources.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
-ARCHIVES_DIR="$1"
-SOURCES_DIR="$2"
+archive="$1"
+outdir="$2"
 
-mkdir -p "$SOURCES_DIR"
-
-for archive in "$ARCHIVES_DIR"/????.*
-do
-  mime_type=$(file --brief --uncompress --mime-type "$archive")
-  case "$mime_type" in
-    'application/x-tar')
-      filename=$(basename -- "$archive")
-      outdir="$SOURCES_DIR/$filename"
-      mkdir -p "$outdir"
-      tar -xf "$archive" -C "$outdir"
-      ;;
-    'text/x-tex')
-      filename=$(basename -- "$archive")
-      outdir="$SOURCES_DIR/$filename"
-      mkdir -p "$outdir"
-      gunzip -c "$archive" > "$outdir/main.tex"
-      ;;
-    'application/pdf')
-      echo "File '$archive' is a PDF file, not a LaTeX source code. Skipping" 1>&2
-      ;;
-    *)
-      echo "File '$archive' is of unknown type '$mime_type'. Skipping" 1>&2
-      ;;
-  esac
-done
+mime_type=$(file --brief --uncompress --mime-type "$archive")
+case "$mime_type" in
+  'application/x-tar')
+    mkdir -p "$outdir"
+    tar -xf "$archive" -C "$outdir"
+    ;;
+  'text/x-tex')
+    mkdir -p "$outdir"
+    gunzip -c "$archive" > "$outdir/main.tex"
+    ;;
+  'application/pdf')
+    echo "File '$archive' is a PDF file, not a LaTeX source code. Skipping" 1>&2
+    ;;
+  *)
+    echo "File '$archive' is of unknown type '$mime_type'. Skipping" 1>&2
+    ;;
+esac
