2ad717ec9e81f277736bdf73c0f810494439cc5a
Marcin <marcin.kardas@gmail.com>
2019-07-23 17:11:06 +0200

    Use engrafo instead of htlatex

--- latex2html.sh
+++ latex2html.sh
@@ -2,15 +2,8 @@
 OUTNAME="$1"
 echo $OUTNAME
 RO_SOURCE_DIR="/files/ro-source"
-SOURCE_DIR="/files/source"
 OUTPUT_DIR="/files/htmls"
 
-cp -r "$RO_SOURCE_DIR" "$SOURCE_DIR"
-cd "$SOURCE_DIR"
-MAINTEX=$(find . -type f -iname "*.tex" -print0 | xargs -0 grep -l documentclass | head -1)
-echo $MAINTEX
-timeout -s KILL 60 htlatex "$MAINTEX" '' '' '' '-interaction=batchmode'
+timeout -s KILL 120 engrafo "$RO_SOURCE_DIR" /files/output
 
-FILENAME=$(basename $MAINTEX)
-FILENAME="${FILENAME%.tex}.html"
-cp "$SOURCE_DIR/$FILENAME" "$OUTPUT_DIR/$OUTNAME"
+cp /files/output/index.html "$OUTPUT_DIR/$OUTNAME"
