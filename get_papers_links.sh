28e7089c806dd8d5328c58b36074858835acff9a
Marcin <marcin.kardas@gmail.com>
2019-12-03 17:10:55 +0100

    Extraction pipeline

--- get_papers_links.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-jq '.. | select(.sota?) | .sota.rows[] | .paper_url' "$1" | grep arxiv | sed -e 's#"##g' -e 's#http:#https:#' | sort -u
--- sota_extractor2/models/linking/__init__.py
+++
@@ -1,7 +0,0 @@
-from .taxonomy import Taxonomy
-from .linker import Linker
-from .context_search import ContextSearch, DatasetExtractor
-from .proposals_filters import *
-
-__all__ = ["Taxonomy", "Linker", "ContextSearch", "DatasetExtractor", "ProposalsFilter", "NopFilter",
-           "BestResultFilter", "StructurePredictionFilter", "ConfidenceFilter", "CompoundFilter"]
