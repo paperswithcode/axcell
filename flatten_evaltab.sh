10c9e9c0e1828abcfe792ece0b11819d4e48b0d4
Marcin Kardas <mkardas@fb.com>
2020-05-05 17:08:52 +0100

    Remove unused scripts

--- flatten_evaltab.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#  Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-jq -c '.. | select(.datasets?) | .task as $task | .datasets | .[] | .dataset as $dataset | .sota.rows[] | {paper_url, paper_title, model_name} as $paper | .metrics | . as $metrics | keys[] | {dataset: $dataset, metric_name: ., metric_value: $metrics[.], paper_url: $paper.paper_url, paper_title: $paper.paper_title, model_name: $paper.model_name, task: $task }' "$1" | grep arxiv\.org | jq -s '.'
--- get_papers_links.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#  Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-jq '.. | select(.sota?) | .sota.rows[] | .paper_url' "$1" | grep arxiv | sed -e 's#"##g' -e 's#http:#https:#' | sort -u
