250034bb9d297d4ee05d53e60e36096b28d6dea1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 06:53:06 +0000

    Adding tarfile member sanitization to extractall()

--- axcell/helpers/unpack.py
+++ axcell/helpers/unpack.py
@@ -25,7 +25,29 @@ class Unpack:
         if mime == 'application/x-tar':
             dest.mkdir(parents=True, exist_ok=True)
             with tarfile.open(source, "r:*") as tar:
-                tar.extractall(dest)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, dest)
         elif mime == 'text/x-tex':
             dest.mkdir(parents=True, exist_ok=True)
             with gzip.open(source, "rb") as src, open(dest / "main.tex", "wb") as dst:
